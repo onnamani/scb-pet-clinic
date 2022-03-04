3711e02b5ea7f48bae62f407f29b22cd0c2dc5df
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-04 10:27:54 +0100

    Adding the maven release pluggin. closes #3

--- pet-clinic-web/src/test/java/ng/com/smartcity/scbpetclinic/ScbPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/ng/com/smartcity/scbpetclinic/ScbPetClinicApplicationTests.java
@@ -1,5 +1,6 @@
-package scbpetclinic;
+package ng.com.smartcity.scbpetclinic;
 
+import ng.com.smartcity.scbpetclinic.ScbPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
