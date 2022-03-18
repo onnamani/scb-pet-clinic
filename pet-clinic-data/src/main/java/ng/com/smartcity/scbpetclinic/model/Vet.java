02b219ab2d0d9d2c8deec31dcd2323ee591f039e
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-18 11:40:35 +0100

    added speciality entity, associate to vet object. closes #27

--- pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package ng.com.smartcity.scbpetclinic.model;
 
+import java.util.*;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
