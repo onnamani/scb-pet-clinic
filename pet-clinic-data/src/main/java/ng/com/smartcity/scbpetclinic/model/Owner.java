8ddb285e6023c1713ec798d209ac216167669680
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-18 11:36:13 +0100

    adding visit type, update owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package ng.com.smartcity.scbpetclinic.model;
 
+import java.util.*;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
