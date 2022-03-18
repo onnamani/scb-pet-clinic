a778ef4087494029cf980997644fd9771499fa47
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-18 12:13:22 +0100

    added PetTypes to DataLoader. closes #32

--- pet-clinic-web/src/main/java/ng/com/smartcity/scbpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ng/com/smartcity/scbpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package ng.com.smartcity.scbpetclinic.bootstrap;
 
 import ng.com.smartcity.scbpetclinic.model.Owner;
+import ng.com.smartcity.scbpetclinic.model.PetType;
 import ng.com.smartcity.scbpetclinic.model.Vet;
 import ng.com.smartcity.scbpetclinic.services.OwnerService;
+import ng.com.smartcity.scbpetclinic.services.PetTypeService;
 import ng.com.smartcity.scbpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Obinna");
         owner1.setLastName("Nnamani");
