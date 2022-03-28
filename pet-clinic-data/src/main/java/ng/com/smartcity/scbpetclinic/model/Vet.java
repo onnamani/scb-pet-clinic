1d2700fdf21eb0908e9b63485f81572974d92491
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-28 10:07:47 +0100

    updated vet use JPA mappings. closes #37

--- pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package ng.com.smartcity.scbpetclinic.model;
 
+import javax.persistence.*;
 import java.util.*;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+    joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialties() {
