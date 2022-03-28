737dc81fe4c57412ec3ec95c72f7236b1bc42175
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-28 10:14:12 +0100

    updated visits to use JPA mappings. closes #38

--- pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/model/Visit.java
@@ -1,11 +1,20 @@
 package ng.com.smartcity.scbpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.*;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
     private LocalDate date;
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
