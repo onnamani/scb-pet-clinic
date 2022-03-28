a40293b3ff378647f4894453aadbfc15bf191e03
onnamani <68425897+onnamani@users.noreply.github.com>
2022-03-28 13:12:24 +0100

    created pet service for spring data jpa. closes #43

---
+++ pet-clinic-data/src/main/java/ng/com/smartcity/scbpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package ng.com.smartcity.scbpetclinic.services.springdatajpa;
+
+import ng.com.smartcity.scbpetclinic.model.Pet;
+import ng.com.smartcity.scbpetclinic.repositories.PetRepository;
+import ng.com.smartcity.scbpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
