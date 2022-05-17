711a0a90e56291e6304fc844557b846b8ca8e121
onnamani <68425897+onnamani@users.noreply.github.com>
2022-05-17 17:32:58 +0100

    fixed find by like bug. closes #67

--- pet-clinic-web/src/main/java/ng/com/smartcity/scbpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ng/com/smartcity/scbpetclinic/controllers/OwnerController.java
@@ -41,7 +41,7 @@ public class OwnerController {
         if(owner.getLastName() == null)
             owner.setLastName("");
 
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName().trim());
+        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName().trim() + "%");
 
        if(results.isEmpty()) {
            result.rejectValue("lastName", "notFound", "not found");
