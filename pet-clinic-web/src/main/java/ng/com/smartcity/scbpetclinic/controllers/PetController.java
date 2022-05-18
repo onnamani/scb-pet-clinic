289a349c56b23c53978768a17ea3ca6ff92391a4
onnamani <68425897+onnamani@users.noreply.github.com>
2022-05-18 11:59:53 +0100

    adding model attributes for the pet controller. #56

---
+++ pet-clinic-web/src/main/java/ng/com/smartcity/scbpetclinic/controllers/PetController.java
@@ -0,0 +1,42 @@
+package ng.com.smartcity.scbpetclinic.controllers;
+
+import ng.com.smartcity.scbpetclinic.model.Owner;
+import ng.com.smartcity.scbpetclinic.model.PetType;
+import ng.com.smartcity.scbpetclinic.services.OwnerService;
+import ng.com.smartcity.scbpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
+
+    public PetController(PetTypeService petTypeService, OwnerService ownerService) {
+        this.petTypeService = petTypeService;
+        this.ownerService = ownerService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+       return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
